acd063dc217344526353ccaa93583e03086c5ff5
Shivankswami77777 <74890387+Shivankswami77777@users.noreply.github.com>
2021-07-10 15:24:30 +0530

    code commit 3

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,25 @@
 import React from "react";
+import "./Home.css";
+import HomeIcon from "@material-ui/icons/Home";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>This is home section</div>;
+  return (
+    <div className="home__page">
+      <h1>
+        TravClan Assignment
+        <br />
+        Submitted By :- Shvank Swami
+      </h1>
+      <Link style={{ textDecoration: "none" }} to="/customerbids">
+        <Button style={{ color: "#6E3E8B" }} variant="contained">
+          <HomeIcon />
+          Sign in
+        </Button>
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
